d0b3646fb6c4b9ecad272cb1079e36e310251dda
Teddy Wing <git@teddywing.com>
2015-12-31 01:25:49 -0800

    NotesController#index: Remove old code
    The first block here was before we integrated the 'evernote' wrapper gem
    and is no longer used.
    
    The second block was me thinking of getting all the notes for all
    notebooks in the controller but I ended up just doing that in the view.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,14 +3,7 @@ class NotesController < ApplicationController
     access_token = 'S=s1:U=91e2f:E=151f92661a2:C=151f4000220:P=185:A=evernotesandbox199:V=2:H=5777502290baf1ae1b36ad6254592258'
     notestore_url = 'https://sandbox.evernote.com/shard/s1/notestore'
 
-    # e = EvernoteService.new(auth_token, notestore_url)
-    # @notebooks = e.notebooks
-
-    # @notes = @notebooks.collect { |b| e.notes_from_notebook(b) }
-
     note_store = Evernote::NoteStore.new(notestore_url, access_token)
-
     @notebooks = note_store.notebooks
-    # @notes = @notebooks.collect { |b| b.notes }
   end
 end
