122f68c5cec937953dab0d42d0a7a0fa656806d7
Teddy Wing <git@teddywing.com>
2015-12-30 17:38:41 -0800

    SessionsController: Add sample code provided by OmniAuth
    This is the sample callback code to get access to the OAuth auth hash.
    
    A new route specifies that `SessionsController#create` should handle the
    OAuth callback coming from Evernote.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   # get '/auth/evernote' => 'auth/evernote#index'
+  get '/auth/evernote/callback' => 'sessions#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
