4b945cc8801549990fde0b4dfc1c39e20d9b23c3
Teddy Wing <git@teddywing.com>
2015-12-31 01:18:58 -0800

    Remove Auth::EvernoteController
    No longer used now that we're authenticating with OmniAuth.

--- app/helpers/auth/evernote_helper.rb
+++
@@ -1,2 +0,0 @@
-module Auth::EvernoteHelper
-end
--- test/controllers/auth/evernote_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Auth::EvernoteControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
