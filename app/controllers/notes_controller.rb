069fb91ed6f134c854fda3095d37d69e49c51b0b
Teddy Wing <git@teddywing.com>
2015-12-31 01:20:07 -0800

    Remote services/evernote.rb
    This file is no longer used now that we're leveraging the cgs/kipcole9
    'evernote' gem (https://github.com/teddywing/evernote).

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,3 @@
-# require_relative '../services/evernote'
-
 class NotesController < ApplicationController
   def index
     access_token = 'S=s1:U=91e2f:E=151f92661a2:C=151f4000220:P=185:A=evernotesandbox199:V=2:H=5777502290baf1ae1b36ad6254592258'
