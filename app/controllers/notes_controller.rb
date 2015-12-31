d5407a3c9af0002b2290d96da7593dee98dcb6c4
Teddy Wing <git@teddywing.com>
2015-12-30 18:10:24 -0800

    Trying to get notes from notebooks
    Want to be able to get all notes from a given Evernote notebook. But
    it's proving harder than I thought. Geez, the Evernote SDK is so obtuse.
    Going to look for a different way to do this.
    
    Refactored the `EvernoteService` a bit in the process.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,8 +4,10 @@ class NotesController < ApplicationController
   def index
     auth_token = 'S=s1:U=91e2f:E=151f92661a2:C=151f4000220:P=185:A=evernotesandbox199:V=2:H=5777502290baf1ae1b36ad6254592258'
     notestore_url = 'https://sandbox.evernote.com/shard/s1/notestore'
-    
+
     e = EvernoteService.new(auth_token, notestore_url)
     @notebooks = e.notebooks
+
+    # @notes = @notebooks.collect { |b| e.notes_from_notebook(b) }
   end
 end
--- app/services/evernote.rb
+++ app/services/evernote.rb
@@ -2,12 +2,22 @@ class EvernoteService
   def initialize(auth_token, notestore_url)
     @auth_token = auth_token
     @notestore_url = notestore_url
+
+    noteStoreTransport = Thrift::HTTPClientTransport.new(@notestore_url)
+    noteStoreProtocol = Thrift::BinaryProtocol.new(noteStoreTransport)
+    @note_store = Evernote::EDAM::NoteStore::NoteStore::Client.new(noteStoreProtocol)
   end
 
   def notebooks
-    noteStoreTransport = Thrift::HTTPClientTransport.new(@notestore_url)
-    noteStoreProtocol = Thrift::BinaryProtocol.new(noteStoreTransport)
-    noteStore = Evernote::EDAM::NoteStore::NoteStore::Client.new(noteStoreProtocol)
-    noteStore.listNotebooks(@auth_token)
+    @note_store.listNotebooks(@auth_token)
+  end
+
+  def notes_from_notebook(notebook)
+    @note_store.findNotesMetadata(
+      @auth_token,
+      Evernote::EDAM::NoteStore::NoteFilter.new(:notebookGuid => notebook.guid),
+      0,
+      50,
+    )
   end
 end
