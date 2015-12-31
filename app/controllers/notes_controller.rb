b41a5d1bdde380b698b1123e25532fc26e4bf879
Teddy Wing <git@teddywing.com>
2015-12-30 18:49:21 -0800

    NotesController: Rename `auth_token` to `access_token`
    For consistency with the 'evernote' gem that we're now using.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,7 +2,7 @@ require_relative '../services/evernote'
 
 class NotesController < ApplicationController
   def index
-    auth_token = 'S=s1:U=91e2f:E=151f92661a2:C=151f4000220:P=185:A=evernotesandbox199:V=2:H=5777502290baf1ae1b36ad6254592258'
+    access_token = 'S=s1:U=91e2f:E=151f92661a2:C=151f4000220:P=185:A=evernotesandbox199:V=2:H=5777502290baf1ae1b36ad6254592258'
     notestore_url = 'https://sandbox.evernote.com/shard/s1/notestore'
 
     # e = EvernoteService.new(auth_token, notestore_url)
@@ -10,7 +10,6 @@ class NotesController < ApplicationController
 
     # @notes = @notebooks.collect { |b| e.notes_from_notebook(b) }
 
-    access_token = auth_token
     note_store = EvernoteService::NoteStore.new(notestore_url, access_token)
 
     @notebooks = note_store.notebooks
